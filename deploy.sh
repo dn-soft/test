5356888c3ab92e7ae26cf419fea4d56f21e01bb2
dn-soft <sukim@dnsoft.co.kr>
2020-11-09 17:31:31 +0900

    hi

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 # docker pull test1236.azurecr.io/testimage
-sudo docker login test1236.azurecr.io
-sudo echo test1236
-sudo echo iOj/573TN0jY+mwIT79Hh3bOICI38O7F
-sudo docker pull test1236.azurecr.io/testimage
-sudo docker build --tag testimage/ 
-sudo docker run -p 3000:3000 -d testimage
+docker login test1236.azurecr.io
+docker pull test1236.azurecr.io/testimage:39
+echo ????????????????????????
+docker run -p 3000:3000 -d test1236.azurecr.io/testimage:39
