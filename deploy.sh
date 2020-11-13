4d0557f2f41c81c869c43ee92d6201c84e990792
dn-soft <sukim@dnsoft.co.kr>
2020-11-13 11:55:18 +0900

    444

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-sudo docker login test1236.azurecr.io
-sudo docker stop `docker ps -a -q`
-sudo docker rm `docker ps -qa`
+# sudo docker login test1236.azurecr.io
+# sudo docker stop `docker ps -a -q`
+# sudo docker rm `docker ps -qa`
 
-sudo docker pull test1236.azurecr.io/testimage:latest
-sudo docker run -p 3000:3000 -d test1236.azurecr.io/testimage:latest
+# sudo docker pull test1236.azurecr.io/testimage:latest
+# sudo docker run -p 3000:3000 -d test1236.azurecr.io/testimage:latest
+docker pull test1236.azurecr.io/testimage
