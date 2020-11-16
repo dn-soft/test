3e97019e874f70767d4c5625144ddcf1ab63674b
dn-soft <sukim@dnsoft.co.kr>
2020-11-16 19:52:16 +0900

    3434

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # sudo docker login test1236.azurecr.io
-# sudo docker stop `docker ps -a -q`
-# sudo docker rm `docker ps -qa`
+sudo docker stop `docker ps -a -q`
+sudo docker rm `docker ps -qa`
+sudo export num=$RELEASE_ARTIFACTS__PTEST_BUILDNUMBER
+sudo docker pull test1236.azurecr.io/testimage:$RELEASE_ARTIFACTS__PTEST_BUILDNUMBER
+sudo docker run -p 3000:3000 -d test1236.azurecr.io/testimage:$RELEASE_ARTIFACTS__PTEST_BUILDNUMBER
 
-# sudo docker pull test1236.azurecr.io/testimage:latest
-# sudo docker run -p 3000:3000 -d test1236.azurecr.io/testimage:latest
-docker pull test1236.azurecr.io/testimage
